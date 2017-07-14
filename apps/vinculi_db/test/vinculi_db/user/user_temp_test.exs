c5d4e5a896edf57796b3720c248017af08505efc
Dominique VASSARD <dominique.vassard@gmail.com>
2017-07-14 21:49:01 +0200

    [VINCULI_DB] Fix text extension (.ex instead of .exs)

--- apps/vinculi_db/test/vinculi_db/user/user_temp_test.exs
+++ apps/vinculi_db/test/vinculi_db/user/user_temp_test.exs
@@ -1,4 +1,4 @@
-defmodule VinculiDb.User.UserRepoTest do
+defmodule VinculiDb.User.UserTempTest do
   use VinculiDb.SupportCase
   alias VinculiDb.User.UserTemp
   alias VinculiDb.User.User
@@ -7,7 +7,6 @@ defmodule VinculiDb.User.UserRepoTest do
     attrs = %{first_name: "John", last_name: "Duff",
                   email: "john.duff@email.com", pass: "Gr34tPass!"}
     assert {:ok, insert_result} = UserTemp.add(attrs)
-    IO.puts inspect insert_result
 
     result = Map.put(insert_result, :pass, nil)
 
