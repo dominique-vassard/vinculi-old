bfafba5b435945c6c9b4558d94f66febbe45886c
Dominique VASSARD <dominique.vassard@gmail.com>
2017-07-21 21:23:33 +0200

    Remove sensitive data

--- apps/ars_magica/config/prod.secret.exs
+++ apps/ars_magica/config/prod.secret.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Ars Magica DB configuration
 config :ars_magica, ArsMagica.Repo,
   adapter: Ecto.Adapters.MySQL,
-  username: "arsmagbdd",
-  password: "Nougat13250jhk",
-  hostname: "sqlprive-bc52475-001.privatesql",
+  username: System.get_env("ARS_MAGICA_DB_USER"),
+  password: System.get_env("ARS_MAGICA_DB_PASSWORD"),
+  hostname: System.get_env("ARS_MAGICA_DB_HOST"),
   database: "arsmagbdd"
