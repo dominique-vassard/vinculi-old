b9fadce42e350510ce91ef89f2779c9cedef2b9a
Dominique VASSARD <dominique.vassard@gmail.com>
2017-07-21 20:49:03 +0200

    [ARS_MAGICA] Config for heroku deployment

---
+++ apps/ars_magica/config/prod.secret.exs
@@ -0,0 +1,7 @@
+# Ars Magica DB configuration
+config :ars_magica, ArsMagica.Repo,
+  adapter: Ecto.Adapters.MySQL,
+  username: "arsmagbdd",
+  password: "Nougat13250jhk",
+  hostname: "sqlprive-bc52475-001.privatesql",
+  database: "arsmagbdd"
