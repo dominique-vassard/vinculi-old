dac8762b695f4d0728c8794db9c1a20686d03023
Dominique VASSARD <dominique.vassard@gmail.com>
2017-07-21 20:49:26 +0200

    [VINCULI_API] Config for heroku deployment

--- apps/vinculi_api/config/prod.exs
+++ apps/vinculi_api/config/prod.exs
@@ -15,11 +15,23 @@ use Mix.Config
 # which you typically run after static files are built.
 config :vinculi_api, VinculiApi.Web.Endpoint,
   on_init: {VinculiApi.Web.Endpoint, :load_from_system_env, []},
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  # http: [port: {:system, "PORT"}],
+  # url: [scheme: "https", host: "vinculi.heroku-appcom", port: 443],
+  force_ssl: [host: nil, rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Configure  Neo4J database
+config :bolt_sips, Bolt,
+  hostname: System.get_env("GRAPHENEDB_BOLT_URL"),
+  port: 7687,
+  pool_size: 5,
+  max_overflow: 1,
+  basic_auth: ["username": System.get_env("GRAPHENEDB_BOLT_USER"),
+               "password": System.get_env("GRAPHENEDB_BOLT_PASSWORD")]
 
 # ## SSL Support
 #
@@ -61,4 +73,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
