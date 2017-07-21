3a964237f384afd5e3b39b4c35f510096b014362
Dominique VASSARD <dominique.vassard@gmail.com>
2017-07-21 23:26:55 +0200

    Config fixes

--- apps/vinculi_api/config/prod.exs
+++ apps/vinculi_api/config/prod.exs
@@ -18,7 +18,6 @@ config :vinculi_api, VinculiApi.Web.Endpoint,
   # http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "vinculi.herokuapp.com", port: 443],
   force_ssl: [host: nil, rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
