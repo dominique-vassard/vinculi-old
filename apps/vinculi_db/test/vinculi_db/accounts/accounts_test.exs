5a741b3cdef77c7fde6d947a5bb4fa4e838bf950
Dominique VASSARD <dominique.vassard@gmail.com>
2017-07-28 18:10:00 +0200

    [VINCULI_DB] Improve tests

--- apps/vinculi_db/test/vinculi_db/accounts/accounts_test.exs
+++ apps/vinculi_db/test/vinculi_db/accounts/accounts_test.exs
@@ -3,37 +3,55 @@ defmodule VinculiDb.User.UserRepoTest do
 
   alias VinculiDb.Accounts
 
-  @valid_user_attrs %{first_name: "John", last_name: "Duff",
-                      email: "john.duff@email.com", pass: "Str0ng!On3",
-                      pass_confirmation: "Str0ng!On3"}
+  describe "Test users:" do
+    alias VinculiDb.Accounts.User
 
-  describe "test user signup (databse add): " do
-    test "fail to insert invalid changeset" do
-      attrs = Map.put(@valid_user_attrs, :pass_confirmation, "bad_confirm")
+    @valid_attrs %{first_name: "John", last_name: "Duff",
+                        email: "john.duff@email.com", pass: "Str0ng!On3",
+                        pass_confirmation: "Str0ng!On3"}
+
+    def user_fixture(attrs \\ %{}) do
+      {:ok, user} =
+      attrs
+      |> Enum.into(@valid_attrs)
+      |> Accounts.signup_user()
+
+      user
+    end
+
+    test "get_user/2 returns %User if it exists" do
+      user = user_fixture()
+      user_wo_virtual = Map.merge(user, %{pass: nil, pass_confirmation: nil})
+      assert Accounts.get_user!(user.id) == user_wo_virtual
+    end
+
+    test "get_user/2 raises an Exception if user doesn't exist" do
+      assert_raise Ecto.NoResultsError, fn ->
+        Accounts.get_user!(-65987)
+      end
+    end
+
+    test "signup_user/1 with valid data creates user" do
+      assert {:ok, %User{} = user} = Accounts.signup_user(@valid_attrs)
+      assert %{first_name: "John", last_name: "Duff",
+                      email: "john.duff@email.com", pass: "Str0ng!On3",
+                      pass_confirmation: "Str0ng!On3"} = user
+      assert Accounts.get_user!(user.id)
+    end
+
+    test "signup_user/1 with invalid data returns changeset error" do
+      attrs = Map.put(@valid_attrs, :pass_confirmation, "bad_confirm")
 
       assert {:error, _changeset} = Accounts.signup_user(attrs)
     end
 
-    test "Successful insert with valid changeset" do
-      assert {:ok, user} = Accounts.signup_user(@valid_user_attrs)
-      assert %{first_name: "John", last_name: "Duff",
-                      email: "john.duff@email.com", pass: "Str0ng!On3",
-                      pass_confirmation: "Str0ng!On3"} = user
+    test "change_user/1 returns a user changeset" do
+      user = user_fixture()
+      assert %Ecto.Changeset{} = Accounts.change_user(user)
     end
+
   end
 
 
-  test "Get a user" do
-    {:ok, user} = Accounts.signup_user(@valid_user_attrs)
 
-    res =  Accounts.get_user!(user.id)
-    assert %{first_name: "John", last_name: "Duff",
-                  email: "john.duff@email.com"} = res
-  end
-
-  test "Get non-existing user" do
-    assert_raise Ecto.NoResultsError, fn ->
-      Accounts.get_user!(-65987)
-    end
-  end
 end
