789093de8935d382d45ca4fe8fff79580ed077d5
Dominique VASSARD <dominique.vassard@gmail.com>
2017-07-28 14:20:11 +0200

    [VINULI_API] Update vinculi_db testing (take new structure i naccount)

--- apps/vinculi_api/test/vinculi_api/deps/vinculi_db_test.exs
+++ apps/vinculi_api/test/vinculi_api/deps/vinculi_db_test.exs
@@ -1,16 +1,16 @@
 defmodule VinculiApi.Web.VinculiDbTest do
   use VinculiApi.DataCase
-  alias VinculiDb.User.UserTemp
+  alias VinculiDb.Accounts
 
   test "Add a user to database" do
     attrs = %{first_name: "VinculiApi", last_name: "Duff",
                   email: "john.duff@email.com", pass: "Gr34tPass!",
                   pass_confirmation: "Gr34tPass!"}
-    assert {:ok, insert_result} = UserTemp.add(attrs)
+    assert {:ok, user} = Accounts.signup_user(attrs)
 
-    result = Map.merge(insert_result, %{pass: nil, pass_confirmation: nil})
+    result = Map.merge(user, %{pass: nil, pass_confirmation: nil})
 
-    res = UserTemp.get_by_firstname("VinculiApi")
+    res = Accounts.get_user(user.id)
     assert res == result
   end
 end
